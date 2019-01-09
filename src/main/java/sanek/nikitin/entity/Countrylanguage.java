0349d87a46cb26ce9a43011324853e0df1b1de15
Пользователь2 <2@DESKTOP-B2ETKI2>
2019-01-09 11:46:07 +0300

    1

---
+++ src/main/java/sanek/nikitin/entity/Countrylanguage.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sanek.nikitin.entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Пользователь2
+ */
+@Entity
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Countrylanguage.findAll", query = "SELECT c FROM Countrylanguage c")
+    , @NamedQuery(name = "Countrylanguage.findByCountryCode", query = "SELECT c FROM Countrylanguage c WHERE c.countrylanguagePK.countryCode = :countryCode")
+    , @NamedQuery(name = "Countrylanguage.findByLanguage", query = "SELECT c FROM Countrylanguage c WHERE c.countrylanguagePK.language = :language")
+    , @NamedQuery(name = "Countrylanguage.findByIsOfficial", query = "SELECT c FROM Countrylanguage c WHERE c.isOfficial = :isOfficial")
+    , @NamedQuery(name = "Countrylanguage.findByPercentage", query = "SELECT c FROM Countrylanguage c WHERE c.percentage = :percentage")})
+public class Countrylanguage implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @EmbeddedId
+    protected CountrylanguagePK countrylanguagePK;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 2)
+    private String isOfficial;
+    @Basic(optional = false)
+    @NotNull
+    private float percentage;
+    @JoinColumn(name = "CountryCode", referencedColumnName = "Code", insertable = false, updatable = false)
+    @ManyToOne(optional = false)
+    private Country country;
+
+    public Countrylanguage() {
+    }
+
+    public Countrylanguage(CountrylanguagePK countrylanguagePK) {
+        this.countrylanguagePK = countrylanguagePK;
+    }
+
+    public Countrylanguage(CountrylanguagePK countrylanguagePK, String isOfficial, float percentage) {
+        this.countrylanguagePK = countrylanguagePK;
+        this.isOfficial = isOfficial;
+        this.percentage = percentage;
+    }
+
+    public Countrylanguage(String countryCode, String language) {
+        this.countrylanguagePK = new CountrylanguagePK(countryCode, language);
+    }
+
+    public CountrylanguagePK getCountrylanguagePK() {
+        return countrylanguagePK;
+    }
+
+    public void setCountrylanguagePK(CountrylanguagePK countrylanguagePK) {
+        this.countrylanguagePK = countrylanguagePK;
+    }
+
+    public String getIsOfficial() {
+        return isOfficial;
+    }
+
+    public void setIsOfficial(String isOfficial) {
+        this.isOfficial = isOfficial;
+    }
+
+    public float getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(float percentage) {
+        this.percentage = percentage;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (countrylanguagePK != null ? countrylanguagePK.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Countrylanguage)) {
+            return false;
+        }
+        Countrylanguage other = (Countrylanguage) object;
+        if ((this.countrylanguagePK == null && other.countrylanguagePK != null) || (this.countrylanguagePK != null && !this.countrylanguagePK.equals(other.countrylanguagePK))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "sanek.nikitin.entity.Countrylanguage[ countrylanguagePK=" + countrylanguagePK + " ]";
+    }
+    
+}
